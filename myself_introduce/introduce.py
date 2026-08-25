df0e9f86d841f5dd361bbc7a8edeac07173aed21
saber9050 <1223349392@qq.com>
2026-08-25 17:57:24 +0800

    简单介绍

---
+++ myself_introduce/introduce.py
@@ -0,0 +1,13 @@
+# 自我介绍
+def introduce():
+    name = "saber"
+    age = 22
+    weight = 105
+    goal = "成为一名"
+    goal2 = "优秀的程序员"
+    print("Hello everyone!\n我叫",name)
+    print(f"我今年{age}岁")
+    print("体重%d斤"%weight)
+    print("我的目标是:%s%s"%(goal,goal2))
+
+introduce()

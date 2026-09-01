a146c9cb45d1fd465f1e024c5b12a0aebf3ac52f
saber9050 <1223349392@qq.com>
2026-09-01 20:53:43 +0800

    字典操作

--- chapter5/teset.py
+++ chapter5/teset.py
@@ -8,3 +8,14 @@ print(s1 & s2)
 print(s1 ^ s2)
 s1.add(77)
 print(s1)
+s2 = s1.copy()
+print(s2)
+
+# 字典操作
+d1 = {}
+d2 = dict()
+print(type(d1),type(d2))
+d1["ksl"] = "kjoi"
+print(d1)
+d1.pop("ksl")
+print(d1)

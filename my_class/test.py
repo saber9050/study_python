51ec0a13e6f5d4a81a2920732289a8b25e4b1174
saber9050 <1223349392@qq.com>
2026-09-05 20:51:09 +0800

    类继承

--- my_class/test.py
+++ my_class/test.py
@@ -1,5 +1,6 @@
 class Dog:
     # 构造方法
+    # 注意：后面的类型只是提示，如果类型不一样也不会报错，不会做校验
     def __init__(self,name:str,age:int):
         self.name = name
         self.age = age
@@ -9,3 +10,22 @@ class Dog:
 
 d1 = Dog("大黄",3)
 d1.introduce()
+
+# 临时扩展属性，不会影响类本身
+d1.like = "草莓"
+
+print(d1.like)
+
+
+
+# 类继承
+class YellowDog(Dog):
+    def __init__(self,  name: str, age: int,like: str):
+        super().__init__(name, age)
+        self.like = like
+
+    def my_like(self):
+        print(f"我喜欢{self.like}")
+
+y = YellowDog("橘子狗","i","石头")
+print(y.age)

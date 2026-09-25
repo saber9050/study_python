cce6e259af39cfc6a15769bab7454953cbd0b2cd
saber9050 <1223349392@qq.com>
2026-09-25 20:40:38 +0800

    静态方法

--- my_class/classfun.py
+++ my_class/classfun.py
@@ -2,8 +2,6 @@
 是类方法（或属性）还是实例方法，是看它是给类用还是实例用
 """
 
-
-
 # 类方法
 class Dog:
     name = "大黄"
@@ -19,6 +17,15 @@ class Dog:
     @classmethod
     def new(cls,age):
         return Dog(age)
+
+    # 静态方法：使用@staticmethod装饰的方法， 通常用类来调用
+    # 通常用于定义与类相关的工具方法
+    @staticmethod
+    def is_adult(age):
+        if age > 18:
+            print("成年")
+        else:
+            print("未成年")
 # 用类调用类方法
 Dog.run(3)
 
@@ -31,3 +38,5 @@ print(d1.name)
 
 a = Dog.new(99)
 print(a.__dict__)
+
+a.is_adult(9)
